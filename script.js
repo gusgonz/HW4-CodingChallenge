627caa67c3b0b41ab34e6c827cde579e1d05df20
GusGo <gus.gonzalez.15@gmail.com>
2020-01-04 00:44:18 -0500

    slight beautification

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-
-
+function hideThings() {
+    // Hiding whats there with jQuery
+    $(".quiz-text").hide();
+    $(".main-btns").hide();
+}
 
 
 
@@ -8,29 +11,32 @@
 
 $(".start-quiz-btn").on("click", 
 function startQuiz() {
+    
+    hideThings();
+
     var codingQuiz = $(".coding-quiz");
-
-    // Hiding whats there with jQuery
-    $(".quiz-text").hide();
-    $(".main-btns").hide();
-
-    // Creating and displaying timer with classes everywhere
     var timeLeft = 60;
-    codingQuiz.append($("<div class =timer><p class=timer-text></p><span class=time-left></span></div>"));
-    $(".timer").addClass("row justify-content-end")
-    $(".timer-text").text("Time Remaining:");
-    $(".time-left").text(timeLeft);
+    
+     // Creating and displaying timer with classes everywhere
+     codingQuiz.append($("<div class =timer><p class=timer-text></p><span class=time-left></span></div>"));
+     $(".timer").addClass("row justify-content-end")
+     $(".timer-text").text("Time Remaining:");
+     $(".time-left").text(timeLeft);
+ 
+     // Starts countdown. If the coundown ends, run times up function and end timer function
+     setInterval(function() {
+         timeLeft--;
+          if (timeLeft >= 0) {
+             $(".time-left").text(timeLeft);
+          } else if (timeLeft === 0) {
+             // timesUp();
+             return;
+           }
+         }, 1000);
 
-    // Starts countdown. If the coundown ends, run times up function and end timer function
-    setInterval(function() {
-        timeLeft--;
-         if (timeLeft >= 0) {
-            $(".time-left").text(timeLeft);
-         } else if (timeLeft === 0) {
-            // timesUp();
-            return;
-          }
-        }, 1000);
+    
+
+    
 
 
 
