a879be4ccd3bbe0a85503ee02fb39da3eb1be859
GusGo <gus.gonzalez.15@gmail.com>
2020-01-04 00:57:08 -0500

    added an array holding 4 objects each containing a question, its answer choices, and correct answer to use for my quiz.

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+var questions = [
+    {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+    },
+    {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+    },
+    {
+    title: "Which built-in method adds one or more elements to the end of an array and returns the new length of the array?",
+    choices: ["last()", "put()", "push()", "None of the above"],
+    answer: "push()"
+    },
+    {
+    title: "What is the HTML tag under which one can write the JavaScript code?",
+    choices: ["<javascript>", "<scripted>", "<script>", "<js>"],
+    answer: "<script>"
+    }
+  ];
+
+
 function hideThings() {
     // Hiding whats there with jQuery
     $(".quiz-text").hide();
     $(".main-btns").hide();
 }
-
-
-
-
 
 
 $(".start-quiz-btn").on("click", 
@@ -33,6 +53,8 @@ function startQuiz() {
              return;
            }
          }, 1000);
+
+    
 
     
 
